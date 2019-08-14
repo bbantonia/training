8bbb6a52904b5174231fb55eaf23dc1a642dc872
Antonia Badarau <antoniab@kainos.com>
2019-08-14 15:25:36 +0100

    fix test for Employee min salary

--- src/test/java/EmployeeTest.java
+++ src/test/java/EmployeeTest.java
@@ -35,15 +35,15 @@ class EmployeeTest {
 	void testSalaryMinFailure() {
 		Employee e = new Employee(15, "Henry Banana");
 		assertThrows(IllegalArgumentException.class, () -> {
-			e.setSalary(6999);
+			e.setSalary(9999);
 		});
-		assertNotEquals(6999, e.getSalary());
+		assertNotEquals(9999, e.getSalary());
 	}
 
 	@Test
 	void testSalaryMinSuccess() {
 		Employee e = new Employee(15, "Henry Banana");
-		e.setSalary(7000);
-		assertEquals(7000, e.getSalary());
+		e.setSalary(10000);
+		assertEquals(10000, e.getSalary());
 	}
 }
